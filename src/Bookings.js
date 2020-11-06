23d50b6aa6f91d45d598be1ac8e2f4f1b72fa16f
bipasha39 <physiobipasha39@gmail.com>
2020-11-06 13:45:24 +0100

    cyf hotel lesson 3

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const search = searchVal => {
-    console.info("TO DO!", searchVal);
+  const [bookings, setBookings] = useState([]);
+  const search = (searchInput, event) => {
+    event.preventDefault();
+    console.log("search function", searchInput);
+    console.info("TO DO!", searchInput);
   };
-  const [bookings, setBookings] = useState(FakeBookings);
-  console.log("state", bookings);
+
+  //instruction 16///
+  useEffect(() => {
+    console.log("Fetching data from cyf hotel");
+
+    fetch(`https://cyf-react.glitch.me`)
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
 
   return (
     <div className="App-content">
--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton.js";
 
-const Search = () => {
+const Search = props => {
+  //instruction number 17//
+  const [searchInput, setSearchInput] = useState("");
+  function handleSearchInput(event) {
+    let eventValue = event.target.value;
+    setSearchInput(eventValue);
+    console.log(eventValue);
+  }
+
+  //instruction 18 in <form>//
   return (
     <div className="search">
       <div className="page-header">
@@ -9,14 +18,16 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={props.search}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
                 type="text"
                 id="customerName"
+                value={searchInput}
                 className="form-control"
                 placeholder="Customer name"
+                onChange={handleSearchInput}
               />
             </div>
             <SearchButton />
